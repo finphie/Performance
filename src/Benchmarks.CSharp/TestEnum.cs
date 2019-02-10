2c6ef32a75277503b1277c98e9f847cf15644d75
finphie <19237170+finphie@users.noreply.github.com>
2019-02-10 18:55:18 +0900

    ファイル追加 (#1)
    * EditorConfig追加
    
    * gitignore修正
    
    * プロジェクト追加
    
    * Directory.Build.props追加
    
    * コーティングスタイル関連の設定ファイルを追加
    
    * 名前空間名変更
    
    * ファイル追加
    
    * ファイル修正
    
    * CA1014無効化

---
+++ src/Benchmarks.CSharp/TestEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Benchmarks.CSharp
+{
+    enum TestEnum
+    {
+        [EnumMember(Value = "a")]
+        A,
+
+        [EnumMember(Value = "a")]
+        B
+    }
+}
