46166cf59bf6dddb46b8621b0e661b795855bc81
finphie <19237170+finphie@users.noreply.github.com>
2019-02-10 18:23:02 +0900

    プロジェクト追加

---
+++ src/Benchmarks.CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace BitbankDotNet.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
