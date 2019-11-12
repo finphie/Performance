028c3f8800d2f2f8a602d8f61ed1b339849a5b40
finphie <19237170+finphie@users.noreply.github.com>
2019-11-12 14:45:36 +0900

    IsNullOrEmptyのベンチマーク対象を追加
    ReadOnlySpan<char>

--- src/Benchmarks.CSharp/IsNullOrEmptyBenchmark.cs
+++ src/Benchmarks.CSharp/IsNullOrEmptyBenchmark.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using BenchmarkDotNet.Attributes;
 
@@ -20,27 +21,45 @@ namespace Benchmarks.CSharp
 
         [Benchmark]
         [ArgumentsSource(nameof(Values))]
-        public bool IsNullOrEmpty(string value)
+        public bool IsNullOrEmpty1(string value)
           => value is null || value.Length == 0;
 
         [Benchmark]
         [ArgumentsSource(nameof(Values))]
-        public bool IsNullOrEmpty1(string value)
+        public bool IsNullOrEmpty2(string value)
             => value is null || (uint)value.Length <= 0U;
 
         [Benchmark]
         [ArgumentsSource(nameof(Values))]
-        public bool IsNullOrEmpty2(string value)
+        public bool IsNullOrEmpty3(string value)
             => (value is null || (uint)value.Length <= 0U) ? true : false;
 
         [Benchmark]
         [ArgumentsSource(nameof(Values))]
-        public bool IsNullOrEmpty3(string value)
+        public bool IsNullOrEmpty4(string value)
             => (value?.Length ?? 0) == 0;
 
         [Benchmark]
         [ArgumentsSource(nameof(Values))]
-        public bool IsNullOrEmpty4(string value)
+        public bool IsNullOrEmpty5(string value)
             => (value?.Length ?? 0) == 0 ? true : false;
+
+        [Benchmark]
+        [Arguments("")]
+        [Arguments("abc")]
+        public bool IsNullOrEmptySpan1(ReadOnlySpan<char> value)
+            => value == null || value.Length == 0;
+
+        [Benchmark]
+        [Arguments("")]
+        [Arguments("abc")]
+        public bool IsNullOrEmptySpan2(ReadOnlySpan<char> value)
+            => value == null || (uint)value.Length <= 0U;
+
+        [Benchmark]
+        [Arguments("")]
+        [Arguments("abc")]
+        public bool IsNullOrEmptySpan3(ReadOnlySpan<char> value)
+           => value == null || value.IsEmpty;
     }
 }
