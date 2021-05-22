c3fd4486cf689b8eb40cc96b50fd50103c24852d
finphie <19237170+finphie@users.noreply.github.com>
2021-05-22 10:11:28 +0900

    配列初期化のベンチマークを追加

---
+++ Source/Benchmarks.CSharp/ArrayClearBenchmark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.CSharp
+{
+    [Config(typeof(BenchmarkConfig))]
+    public class ArrayClearBenchmark
+    {
+        byte[] _buffer;
+
+        [Params(10, 100, 1000)]
+        public int Length { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _buffer = new byte[Length];
+            var random = new Random();
+            random.NextBytes(_buffer);
+        }
+
+        [Benchmark]
+        public byte[] ArrayClear()
+        {
+            Array.Clear(_buffer, 0, Length);
+            return _buffer;
+        }
+
+        [Benchmark]
+        public byte[] SpanClear()
+        {
+            _buffer.AsSpan().Clear();
+            return _buffer;
+        }
+    }
+}
