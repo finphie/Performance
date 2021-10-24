0e46217efd04d1c96608e6723d42aaffdfcf4a43
finphie <19237170+finphie@users.noreply.github.com>
2021-10-24 12:41:46 +0900

    トップレベルステートメント (#87)

--- Source/Benchmarks.CSharp/Program.cs
+++ Source/Benchmarks.CSharp/Program.cs
@@ -1,9 +1,3 @@
 ﻿using BenchmarkDotNet.Running;
 
-namespace Benchmarks.CSharp;
-
-class Program
-{
-    static void Main(string[] args)
-        => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-}
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
