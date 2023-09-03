7c663f5e75d385d0343497a8220f65daf8a5dc88
finphie <19237170+finphie@users.noreply.github.com>
2023-09-03 14:26:25 +0900

    バイト配列のハッシュコード計算ベンチマークを追加 (#116)

---
+++ Source/Performance.Benchmarks/ArrayHashCodeBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using CommunityToolkit.HighPerformance;
+using CommunityToolkit.HighPerformance.Helpers;
+
+namespace Performance.Benchmarks;
+
+[Config(typeof(BenchmarkConfig))]
+public class ArrayHashCodeBenchmark
+{
+    byte[] _data = null!;
+
+    [Params(10, 100, 1024)]
+    public int ArraySize { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _data = new byte[ArraySize];
+        Random.Shared.NextBytes(_data);
+    }
+
+    [Benchmark]
+    public int HashCode()
+    {
+        var hash = default(HashCode);
+        hash.AddBytes(_data);
+        return hash.ToHashCode();
+    }
+
+    [Benchmark]
+    public int CommunityToolkitHashCode()
+    {
+        var hash = default(HashCode);
+        hash.Add<byte>(_data);
+        return hash.ToHashCode();
+    }
+
+    [Benchmark]
+    public int CommunityToolkitHashCodeCombine() => HashCode<byte>.Combine(_data);
+
+    [Benchmark]
+    public int CommunityToolkitDjb2HashCode() => _data.GetDjb2HashCode();
+}
